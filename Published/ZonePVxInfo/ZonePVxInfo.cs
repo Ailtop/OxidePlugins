77b34707c547495235ec761a378dc29a810f1b22
Ailtop <443020352@qq.com>
2022-04-16 00:28:28 +0800

    Support more UI options

--- Published/ZonePVxInfo/ZonePVxInfo.cs
+++ Published/ZonePVxInfo/ZonePVxInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -16,9 +17,8 @@ namespace Oxide.Plugins
         #region Fields
 
         [PluginReference] private readonly Plugin ZoneManager, DynamicPVP, RaidableBases, AbandonedBases;
-        private const string UINAME_MAIN = "ZonePVxInfoUI";
-        private bool pvpAll;
-        private string pvpUIJson, pveUIJson, pvpDelayUIJson;
+        private const string UinameMain = "ZonePVxInfoUI";
+        private bool _pvpAll;
 
         private enum PVxType
         {
@@ -34,9 +34,6 @@ namespace Oxide.Plugins
         private void Init()
         {
             AddCovalenceCommand("pvpall", nameof(CmdServerPVx));
-            pvpUIJson = GetUIJson(PVxType.PVP);
-            pveUIJson = GetUIJson(PVxType.PVE);
-            pvpDelayUIJson = GetUIJson(PVxType.PVPDelay);
             if (configData.defaultType == PVxType.PVPDelay)
             {
                 configData.defaultType = PVxType.PVE;
@@ -62,7 +59,7 @@ namespace Oxide.Plugins
         private void OnPlayerConnected(BasePlayer player)
         {
             if (player == null || !player.userID.IsSteamId()) return;
-            if (pvpAll)
+            if (_pvpAll)
             {
                 CreatePVxUI(player, PVxType.PVP);
             }
@@ -76,9 +73,9 @@ namespace Oxide.Plugins
 
         #region ZoneManager
 
-        private string GetZoneName(string zoneID)
+        private string GetZoneName(string zoneId)
         {
-            return (string)ZoneManager.Call("GetZoneName", zoneID);
+            return (string)ZoneManager.Call("GetZoneName", zoneId);
         }
 
         private string[] GetPlayerZoneIDs(BasePlayer player)
@@ -86,9 +83,9 @@ namespace Oxide.Plugins
             return (string[])ZoneManager.Call("GetPlayerZoneIDs", player);
         }
 
-        private float GetZoneRadius(string zoneID)
+        private float GetZoneRadius(string zoneId)
         {
-            var obj = ZoneManager.Call("GetZoneRadius", zoneID); ;
+            var obj = ZoneManager.Call("GetZoneRadius", zoneId); ;
             if (obj is float)
             {
                 return (float)obj;
@@ -96,9 +93,9 @@ namespace Oxide.Plugins
             return 0f;
         }
 
-        private Vector3 GetZoneSize(string zoneID)
+        private Vector3 GetZoneSize(string zoneId)
         {
-            var obj = ZoneManager.Call("GetZoneSize", zoneID); ;
+            var obj = ZoneManager.Call("GetZoneSize", zoneId); ;
             if (obj is Vector3)
             {
                 return (Vector3)obj;
@@ -106,19 +103,19 @@ namespace Oxide.Plugins
             return Vector3.zero;
         }
 
-        private void OnEnterZone(string zoneID, BasePlayer player)
+        private void OnEnterZone(string zoneId, BasePlayer player)
         {
             NextTick(() => CheckPlayerZone(player));
         }
 
-        private void OnExitZone(string zoneID, BasePlayer player)
+        private void OnExitZone(string zoneId, BasePlayer player)
         {
             NextTick(() => CheckPlayerZone(player));
         }
 
         private void CheckPlayerZone(BasePlayer player, bool checkPVPDelay = true)
         {
-            if (pvpAll || player == null || !player.IsConnected || !player.userID.IsSteamId()) return;
+            if (_pvpAll || player == null || !player.IsConnected || !player.userID.IsSteamId()) return;
             if (checkPVPDelay && IsPlayerInPVPDelay(player.userID))
             {
                 CreatePVxUI(player, PVxType.PVPDelay);
@@ -158,19 +155,19 @@ namespace Oxide.Plugins
             float radius = float.MaxValue;
             string smallest = null;
             var zoneIDs = GetPlayerZoneIDs(player);
-            foreach (var zoneID in zoneIDs)
+            foreach (var zoneId in zoneIDs)
             {
-                var zoneName = GetZoneName(zoneID);
+                var zoneName = GetZoneName(zoneId);
                 if (string.IsNullOrEmpty(zoneName)) continue;
                 float zoneRadius;
-                var zoneSize = GetZoneSize(zoneID);
+                var zoneSize = GetZoneSize(zoneId);
                 if (zoneSize != Vector3.zero)
                 {
                     zoneRadius = (zoneSize.x + zoneSize.z) / 2;
                 }
                 else
                 {
-                    zoneRadius = GetZoneRadius(zoneID);
+                    zoneRadius = GetZoneRadius(zoneId);
                 }
                 if (zoneRadius <= 0f)
                 {
@@ -191,7 +188,7 @@ namespace Oxide.Plugins
 
         private void OnPlayerEnteredRaidableBase(BasePlayer player, Vector3 location, bool allowPVP)
         {
-            if (pvpAll) return;
+            if (_pvpAll) return;
             CreatePVxUI(player, allowPVP ? PVxType.PVP : PVxType.PVE);
         }
 
@@ -214,7 +211,7 @@ namespace Oxide.Plugins
 
         private void OnPlayerEnteredAbandonedBase(BasePlayer player, Vector3 location, bool allowPVP)
         {
-            if (pvpAll) return;
+            if (_pvpAll) return;
             CreatePVxUI(player, allowPVP ? PVxType.PVP : PVxType.PVE);
         }
 
@@ -237,7 +234,7 @@ namespace Oxide.Plugins
 
         private void OnPlayerEnterPVPBubble(TrainEngine trainEngine, BasePlayer player)
         {
-            if (pvpAll) return;
+            if (_pvpAll) return;
             CreatePVxUI(player, PVxType.PVP);
         }
 
@@ -258,9 +255,9 @@ namespace Oxide.Plugins
 
         #region PVPDelay
 
-        private void OnPlayerRemovedFromPVPDelay(ulong playerID, string zoneID) // DynamicPVP
+        private void OnPlayerRemovedFromPVPDelay(ulong playerId, string zoneId) // DynamicPVP
         {
-            var player = BasePlayer.FindByID(playerID);
+            var player = BasePlayer.FindByID(playerId);
             if (player == null) return;
             CheckPlayerZone(player, false);
         }
@@ -310,7 +307,7 @@ namespace Oxide.Plugins
                 case "0":
                 case "off":
                 case "false":
-                    pvpAll = false;
+                    _pvpAll = false;
                     foreach (var player in BasePlayer.activePlayerList)
                     {
                         CheckPlayerZone(player);
@@ -320,7 +317,7 @@ namespace Oxide.Plugins
                 case "1":
                 case "on":
                 case "true":
-                    pvpAll = true;
+                    _pvpAll = true;
                     foreach (var player in BasePlayer.activePlayerList)
                     {
                         CreatePVxUI(player, PVxType.PVP);
@@ -333,93 +330,62 @@ namespace Oxide.Plugins
 
         #region UI
 
-        private string GetUIJson(PVxType type)
+        private void CreatePVxUI(BasePlayer player, PVxType type)
         {
-            int textSize;
-            string zoneText, zoneColor, textColor;
-            switch (type)
+            UiSettings settings;
+            if (!configData.UISettings.TryGetValue(type, out settings) || string.IsNullOrEmpty(settings.Json))
             {
-                case PVxType.PVE:
-                    zoneText = "PVE";
-                    zoneColor = configData.pveColor;
-                    textColor = configData.pveTextColor;
-                    textSize = configData.textSize;
-                    break;
-
-                case PVxType.PVP:
-                    zoneText = "PVP";
-                    zoneColor = configData.pvpColor;
-                    textColor = configData.pvpTextColor;
-                    textSize = configData.textSize;
-                    break;
-
-                case PVxType.PVPDelay:
-                    zoneText = "PVP Delay";
-                    zoneColor = configData.pvpDelayColor;
-                    textColor = configData.pvpDelayTextColor;
-                    textSize = configData.pvpDelayTextSize;
-                    break;
-
-                default: return null;
+                return;
             }
+            CuiHelper.DestroyUi(player, UinameMain);
+            CuiHelper.AddUi(player, settings.Json);
+        }
 
+        private static void DestroyUI(BasePlayer player)
+        {
+            CuiHelper.DestroyUi(player, UinameMain);
+        }
+
+        private static string GetCuiJson(UiSettings settings)
+        {
             return new CuiElementContainer
             {
                 {
                     new CuiPanel
                     {
-                        Image = {Color = zoneColor},
+                        Image =
+                        {
+                            Color = settings.BackgroundColor,
+                            FadeIn = settings.FadeIn,
+                        },
                         RectTransform =
                         {
-                            AnchorMin = configData.minAnchor,
-                            AnchorMax = configData.maxAnchor,
-                            OffsetMin = configData.minOffset,
-                            OffsetMax = configData.maxOffset
-                        }
+                            AnchorMin = settings.MinAnchor,
+                            AnchorMax = settings.MaxAnchor,
+                            OffsetMin = settings.MinOffset,
+                            OffsetMax = settings.MaxOffset,
+                        },
+                        FadeOut = settings.FadeOut,
                     },
-                    configData.layer, UINAME_MAIN
+                    settings.Layer, UinameMain
                 },
                 {
                     new CuiLabel
                     {
                         Text =
                         {
-                            Text = zoneText, FontSize = textSize, Align = TextAnchor.MiddleCenter,
-                            Color = textColor
+                            Text = settings.Text,
+                            FontSize = settings.TextSize,
+                            Align = TextAnchor.MiddleCenter,
+                            Color = settings.TextColor,
+                            FadeIn = settings.FadeIn,
                         },
-                        RectTransform = {AnchorMin = "0.05 0.05", AnchorMax = "0.95 0.95"}
+                        RectTransform = {AnchorMin = "0.05 0.05", AnchorMax = "0.95 0.95"},
+                        FadeOut = settings.FadeOut,
                     },
-                    UINAME_MAIN, CuiHelper.GetGuid()
+                    UinameMain, CuiHelper.GetGuid()
                 }
             }.ToJson();
-        }
-
-        private void CreatePVxUI(BasePlayer player, PVxType type)
-        {
-            string uiJson;
-            switch (type)
-            {
-                case PVxType.PVE:
-                    uiJson = pveUIJson;
-                    break;
-
-                case PVxType.PVP:
-                    uiJson = pvpUIJson;
-                    break;
-
-                case PVxType.PVPDelay:
-                    uiJson = pvpDelayUIJson;
-                    break;
-
-                default: return;
-            }
-            CuiHelper.DestroyUi(player, UINAME_MAIN);
-            CuiHelper.AddUi(player, uiJson);
-        }
-
-        private static void DestroyUI(BasePlayer player)
-        {
-            CuiHelper.DestroyUi(player, UINAME_MAIN);
         }
 
         #endregion UI
@@ -437,44 +403,82 @@ namespace Oxide.Plugins
             [JsonProperty(PropertyName = "Server Default PVx (pvp or pve)")]
             public PVxType defaultType = PVxType.PVE;
 
-            [JsonProperty(PropertyName = "UI - Layer")]
-            public string layer = "Hud";
+            [JsonProperty(PropertyName = "Pvx UI Settings")]
+            public Dictionary<PVxType, UiSettings> UISettings { get; set; } = new Dictionary<PVxType, UiSettings>
+            {
+                [PVxType.PVE] = new UiSettings
+                {
+                    Text = "PVE",
+                    TextSize = 14,
+                    TextColor = "1 1 1 1",
+                    BackgroundColor = "0.3 0.8 0.1 0.8"
+                },
+                [PVxType.PVP] = new UiSettings
+                {
+                    Text = "PVP",
+                    TextSize = 14,
+                    TextColor = "1 1 1 1",
+                    BackgroundColor = "0.8 0.2 0.2 0.8"
+                },
+                [PVxType.PVPDelay] = new UiSettings
+                {
+                    Text = "PVP Delay",
+                    TextSize = 12,
+                    TextColor = "1 1 1 1",
+                    BackgroundColor = "0.8 0.5 0.1 0.8"
+                },
+            };
+        }
 
-            [JsonProperty(PropertyName = "UI - PVP Background Color")]
-            public string pvpColor = "0.8 0.2 0.2 0.8";
+        private class UiSettings
+        {
+            [JsonProperty(PropertyName = "Min Anchor")]
+            public string MinAnchor { get; set; } = "0.5 0";
 
-            [JsonProperty(PropertyName = "UI - PVP Delay Background Color")]
-            public string pvpDelayColor = "0.8 0.5 0.1 0.8";
+            [JsonProperty(PropertyName = "Max Anchor")]
+            public string MaxAnchor { get; set; } = "0.5 0";
 
-            [JsonProperty(PropertyName = "UI - PVE Background Color")]
-            public string pveColor = "0.3 0.8 0.1 0.8";
+            [JsonProperty(PropertyName = "Min Offset")]
+            public string MinOffset { get; set; } = "190 30";
 
-            [JsonProperty(PropertyName = "UI - PVP Text Color")]
-            public string pvpTextColor = "1 1 1 1";
+            [JsonProperty(PropertyName = "Max Offset")]
+            public string MaxOffset { get; set; } = "250 60";
 
-            [JsonProperty(PropertyName = "UI - PVP Delay Text Color")]
-            public string pvpDelayTextColor = "1 1 1 1";
+            [JsonProperty(PropertyName = "Layer")]
+            public string Layer { get; set; } = "Hud";
 
-            [JsonProperty(PropertyName = "UI - PVE Text Color")]
-            public string pveTextColor = "1 1 1 1";
+            [JsonProperty(PropertyName = "Text")]
+            public string Text { get; set; } = "PVP";
 
-            [JsonProperty(PropertyName = "UI - Text Size")]
-            public int textSize = 14;
+            [JsonProperty(PropertyName = "Text Size")]
+            public int TextSize { get; set; } = 12;
 
-            [JsonProperty(PropertyName = "UI - PVP Delay Text Size")]
-            public int pvpDelayTextSize = 12;
+            [JsonProperty(PropertyName = "Text Color")]
+            public string TextColor { get; set; } = "1 1 1 1";
 
-            [JsonProperty(PropertyName = "UI - Min Anchor")]
-            public string minAnchor = "0.5 0";
+            [JsonProperty(PropertyName = "Background Color")]
+            public string BackgroundColor { get; set; } = "0.8 0.5 0.1 0.8";
 
-            [JsonProperty(PropertyName = "UI - Max Anchor")]
-            public string maxAnchor = "0.5 0";
+            [JsonProperty(PropertyName = "Fade In")]
+            public float FadeIn { get; set; } = 0.25f;
 
-            [JsonProperty(PropertyName = "UI - Min Offset")]
-            public string minOffset = "190 30";
+            [JsonProperty(PropertyName = "Fade Out")]
+            public float FadeOut { get; set; } = 0.25f;
 
-            [JsonProperty(PropertyName = "UI - Max Offset")]
-            public string maxOffset = "250 60";
+            private string _json;
+
+            [JsonIgnore]
+            public string Json
+            {
+                get
+                {
+                    if (string.IsNullOrEmpty(_json))
+                    {
+                        _json = GetCuiJson(this);
+                    }
+                    return _json;
+                }
+            }
         }
 
         protected override void LoadConfig()
